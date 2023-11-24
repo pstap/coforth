e9179740a32b9e1f1b0034f3c8d849cd75bdc994
Peter Stapleton <peter.stapleton@protonmail.com>
2023-11-23 23:39:31 -0800

    Added print_cstr routine
    Added `print_cstr` to print a null terminated string.
    Mainly just a test to make sure the stack pointer is ok to
    call and return from routines and to use local labels.

--- coforth.s
+++ coforth.s
@@ -1,25 +1,49 @@
+# coforth.s
+# coffee forth
+
+# riscv32 calling conventions
+# Register Name(s)      Usage
+# x0/zero               Always holds 0
+# ra                    the return address
+# sp                    the address of the boundary of the stack
+# t0-t6                 temporary values that do not persist after function calls
+# s0-s11                values that persist after function calls
+# a0-a1                 first two arguments to the function or the return values
+# a2-a7                 any remaining arguments
+
 .global _start
 
 .equ UART_OUTPUT, 0x10000000
 	
-_start:    
-  li    s1, UART_OUTPUT         # UART output register
-  la    s2, hello               # load string start addr into s2
-  addi  s3, s2, 13              # set up string end addr in s3
-	
-
-loop:
-  lb    s4, 0(s2)               # load next byte at s2 into s4
-  sb    s4, 0(s1)               # write byte to UART register
-  addi  s2, s2, 1               # increase s2
-  blt   s2, s3, loop            # branch back until end addr (s3) reached
+_start: 
+  la    a0, hello
+  call  print_cstr
+  la    a0, eop
+  call  print_cstr
+  j     forever
 
 forever:
   wfi
   j     forever
 
 
+print_cstr: 
+  # a0 <- cstr address, used as char ptr
+  li    t0, UART_OUTPUT         # t0 <- UART output addr
+1:                              # LOOP:
+  lb    t1, 0(a0)               # t1 <- current char
+  beq   t1, zero, 2f            # return if we see null char
+  sb    t1, 0(t0)               # output current char
+  addi  a0, a0, 1               # increment char ptr
+  j     1b
+2:                              # RET:
+  ret
+  
 .section .data
 
 hello:
-  .string "hello world!\n"
+  .asciz "hello from coffee forth!\n"
+
+eop:  
+  .asciz "jumping to infinite loop\n"
+
