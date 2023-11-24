e09a0a109be5f5fec131e0287c7f1502d19be574
Peter Stapleton <peter.stapleton@protonmail.com>
2023-11-24 10:07:22 -0800

    Echo Loop Main
    Made the main a program echo whatever is written to UART0.
    This is mainly to test the UART driver layer

--- coforth.s
+++ coforth.s
@@ -1,6 +1,10 @@
 # coforth.s
 # coffee forth
 
+# Useful links
+# qemu riscv memory map https://github.com/qemu/qemu/blob/master/hw/riscv/virt.c
+# NS16550A Datasheet    https://datasheetspdf.com/pdf-file/605590/NationalSemiconductor/NS16550A/1
+
 # riscv32 calling conventions
 # Register Name(s)      Usage
 # x0/zero               Always holds 0
@@ -11,25 +15,36 @@
 # a0-a1                 first two arguments to the function or the return values
 # a2-a7                 any remaining arguments
 
-.global _start
+# coforth constants
+.equ TOS, s0            # this register always stores the top of the data stack
+.equ RS,  s11           # this register stores the top of the return stack
 
-.equ UART_OUTPUT, 0x10000000
-	
+# uart registers
+.equ UART0, 0x10000000
+
+# outer interpreter
+.global _start	
 _start: 
-  la    a0, hello
+  la    a0, hello       # print welcome messagen
   call  print_cstr
-  la    a0, eop
+  
+1:                      # ECHO LOOP:
+  call  getchar         # get a char
+  beq   a0, zero, 1b    # loop back if we didn't get a char
+  call  putchar
+  la    a0, nl
   call  print_cstr
-  j     forever
+  j     1b              # loop back
 
 forever:
   wfi
   j     forever
 
-
+# output procedures
 print_cstr: 
   # a0 <- cstr address, used as char ptr
-  li    t0, UART_OUTPUT         # t0 <- UART output addr
+  # NOTE: putchar is inlined here
+  li    t0, UART0               # t0 <- UART output addr
 1:                              # LOOP:
   lb    t1, 0(a0)               # t1 <- current char
   beq   t1, zero, 2f            # return if we see null char
@@ -38,12 +53,34 @@ print_cstr:
   j     1b
 2:                              # RET:
   ret
+
+putchar:  
+  # a0 <- char to output to UART
+  li    t0, UART0               # t0 <- UART output addr
+  sb    a0, 0(t0)               # write low byte of t0 to UART
+  ret
+
+# TODO: replace magic numbers with constants
+getchar:  
+  li    t0, UART0               # to <- UART addr
+  lbu   t1, 0x5(t0)             # t1 <- line status
+  andi  t1, t1, 0x1             # lowest bit indicates if char is available
+  addi  a0, zero, 0             # store 0 in return register
+  beqz  t1, 1f                  # return early if no char is ready
+  lbu   a0, (t0)                # load byte from UART0
+1:                              # RET:
+  ret
   
 .section .data
 
 hello:
-  .asciz "hello from coffee forth!\n"
+  .asciz "hello from coffee forth!\r\n"
 
 eop:  
-  .asciz "jumping to infinite loop\n"
+  .asciz "jumping to infinite loop\r\n"
 
+msg:  
+  .asciz "got: "
+
+nl: 
+  .asciz "\r\n"
