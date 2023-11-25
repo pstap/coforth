a3203085ab3b8a6e5ba9e25002ca9b41079b2a27
Peter Stapleton <peter.stapleton@protonmail.com>
2023-11-25 11:25:27 -0800

    Some cleanup

--- coforth.s
+++ coforth.s
@@ -15,17 +15,15 @@
 # outer interpreter
 .global _start	
 _start: 
+  li    sp, 0x88000000          # initialize sp, NOTE: specific to qemu virt machine
   la    a0, hello               # print welcome message
   call  print_cstr
-  li    sp, 0x88000000
 1:                              # ECHO LOOP:
-  mv    a0, sp
-  call  print_word
   call  key
   call  putchar
   la    a0, nl
   call  print_cstr
-  j     1b                      # loop back
+  j     1b
 
 forever:
   wfi
@@ -33,28 +31,28 @@ forever:
 
 # output procedures
 print_cstr:                     # print_cstr: print a null terminated string to UART0
-                                # a0 <- cstr address, used as char ptr
-  li    t0, UART0               # t0 <- UART output addr
+                                # a0 := ! cstr address, used as char ptr
+  li    t0, UART0               # t0 := 0 UART output addr
 1:                              # LOOP:
-  lb    t1, 0(a0)               # t1 <- current char
+  lb    t1, 0(a0)               # t1 := ! current char
   beq   t1, zero, 2f            # return if we see null char
   sb    t1, 0(t0)               # output current char
   addi  a0, a0, 1               # increment char ptr
   j     1b
-2:                              # RET:
+2:
   ret
 
 putchar:                        # putchar: output a char to UART0
-                                # a0 <- char to output to UART
-  li    t6, UART0               # t6 <- UART output addr
-  sb    a0, 0(t6)               # write low byte of t6 to UART
+                                # a0 := 0 char to output to UART
+  li    t0, UART0               # t0 := 0 UART output addr
+  sb    a0, 0(t0)               # write low byte of t0 to UART
   ret
 
 getchar:                        # getchar: get a single char from UART0.
                                 # returns 0 if there's no char available
                                 # TODO: replace magic numbers with constants
-  li    t0, UART0               # to <- UART addr
-  lbu   t1, 0x5(t0)             # t1 <- line status
+  li    t0, UART0               # to := 0 UART addr
+  lbu   t1, 0x5(t0)             # t1 := ! line status
   andi  t1, t1, 0x1             # lowest bit indicates if char is available
   addi  a0, zero, 0             # store 0 in return register
   beqz  t1, 1f                  # return early if no char is ready
@@ -63,40 +61,47 @@ getchar:                        # getchar: get a single char from UART0.
   ret
 
 key:                            # key: blocking version of getchar. spin until we get a character
-  addi  sp, sp, -4              # alloc stack space for ra
+  addi  sp, sp, -16             # alloc stack space for ra
   sw    ra, 0(sp)               # store ra
 1:                              # LOOP:
   call  getchar                 # returns char in a0
   beqz  a0, 1b                  # loop back if we didn't get a character
   lw    ra, 0(sp)               # restore ra from stack
-  addi  sp, sp, 4               # dealloc stack
+  addi  sp, sp, 16              # dealloc stack
   ret                           # RET: a0 holds the char
 
 
 print_word:                     # print_word: prints word in a0 in hex
-  addi  sp, sp, -4              # alloc stack space for ra
-  sw    ra, 0(sp)               # store ra
-  mv    t0, a0                  # t0 <- ! original argument
-  li    t1, 0xF0000000
-  la    t2, hextbl              # t2 <- 0 hex table
-  li    t3, 28                  # t3 <- ! shift amount
-1:                              # LOOP:
-  and   a0, a0, t1              # apply mask to a0
-  srl   a0, a0, t3              # shift over masked nibble to LSB
-  add   a0, a0, t2              # get address of character in table
+  addi  sp, sp, -32             # preamble:
+  sw    s0,  0(sp)              
+  sw    s1,  4(sp)
+  sw    s2,  8(sp)
+  sw    s3, 12(sp)
+  sw    ra, 16(sp)
+                                # body:
+  mv    s0, a0                  # s0 := 0 original argument
+  li    s1, 0xF0000000          # s1 := ! mask 
+  la    s2, hextbl              # s2 := 0 hex table
+  li    s3, 28                  # s3 := ! shift amount
+1:
+  and   a0, a0, s1              # apply mask to a0
+  srl   a0, a0, s3              # shift over masked nibble to LSB
+  add   a0, a0, s2              # get address of character in table
   lb    a0, 0(a0)               # a0 <- hextbl[a0]
   call  putchar                 # print out char
-  beqz  t3, 2f                  # jump to RET if done
-  srli  t1, t1, 4               # shift mask over
-  addi  t3, t3, -4              # subtract 4 from shift amount
-  mv    a0, t0                  # restore original number
+  beqz  s3, 2f                  # return if done
+  srli  s1, s1, 4               # shift mask over
+  addi  s3, s3, -4              # subtract 4 from shift amount
+  mv    a0, s0                  # restore original number
   j     1b
-2:                              # RET:
-  lw    ra, 0(sp)               # restore ra from stack
-  addi  sp, sp, 4               # dealloc stack
+2:                              # epilogue:
+  lw    s0,  0(sp)              
+  lw    s1,  4(sp)
+  lw    s2,  8(sp)
+  lw    s3, 12(sp)
+  lw    ra, 16(sp)
+  addi  sp, sp, 32
   ret
-
-  
 
 .section .data
 
