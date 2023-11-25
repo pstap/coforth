e4a06f9dd300c156abe4546c2ceff2f55c0720fc
Peter Stapleton <peter.stapleton@protonmail.com>
2023-11-24 17:23:37 -0800

    Debugging stack issues
    Stack doesn't seem to work. Wrote a debugging routine
    to print a0 in hex to the UART

--- coforth.s
+++ coforth.s
@@ -5,16 +5,6 @@
 # qemu riscv memory map https://github.com/qemu/qemu/blob/master/hw/riscv/virt.c
 # NS16550A Datasheet    https://datasheetspdf.com/pdf-file/605590/NationalSemiconductor/NS16550A/1
 
-# riscv32 calling conventions
-# Register Name(s)      Usage
-# x0/zero               Always holds 0
-# ra                    the return address
-# sp                    the address of the boundary of the stack
-# t0-t6                 temporary values that do not persist after function calls
-# s0-s11                values that persist after function calls
-# a0-a1                 first two arguments to the function or the return values
-# a2-a7                 any remaining arguments
-
 # coforth constants
 .equ TOS, s0            # this register always stores the top of the data stack
 .equ RS,  s11           # this register stores the top of the return stack
@@ -25,25 +15,25 @@
 # outer interpreter
 .global _start	
 _start: 
-  la    a0, hello       # print welcome messagen
+  la    a0, hello               # print welcome message
   call  print_cstr
-  
-1:                      # ECHO LOOP:
-  call  getchar         # get a char
-  beq   a0, zero, 1b    # loop back if we didn't get a char
+  li    sp, 0x3242
+1:                              # ECHO LOOP:
+  mv    a0, sp
+  call  print_word
+  call  key
   call  putchar
   la    a0, nl
   call  print_cstr
-  j     1b              # loop back
+  j     1b                      # loop back
 
 forever:
   wfi
   j     forever
 
 # output procedures
-print_cstr: 
-  # a0 <- cstr address, used as char ptr
-  # NOTE: putchar is inlined here
+print_cstr:                     # print_cstr: print a null terminated string to UART0
+                                # a0 <- cstr address, used as char ptr
   li    t0, UART0               # t0 <- UART output addr
 1:                              # LOOP:
   lb    t1, 0(a0)               # t1 <- current char
@@ -54,14 +44,15 @@ print_cstr:
 2:                              # RET:
   ret
 
-putchar:  
-  # a0 <- char to output to UART
-  li    t0, UART0               # t0 <- UART output addr
-  sb    a0, 0(t0)               # write low byte of t0 to UART
+putchar:                        # putchar: output a char to UART0
+                                # a0 <- char to output to UART
+  li    t6, UART0               # t6 <- UART output addr
+  sb    a0, 0(t6)               # write low byte of t6 to UART
   ret
 
-# TODO: replace magic numbers with constants
-getchar:  
+getchar:                        # getchar: get a single char from UART0.
+                                # returns 0 if there's no char available
+                                # TODO: replace magic numbers with constants
   li    t0, UART0               # to <- UART addr
   lbu   t1, 0x5(t0)             # t1 <- line status
   andi  t1, t1, 0x1             # lowest bit indicates if char is available
@@ -70,7 +61,40 @@ getchar:
   lbu   a0, (t0)                # load byte from UART0
 1:                              # RET:
   ret
+
+key:                            # key: blocking version of getchar. spin until we get a character
+                                # TODO is stack usage broken?
+  mv    t2, ra                  # store ra in t2 for now, we know getchar won't clobber it
+1:                              # LOOP:
+  call  getchar                 # returns char in a0
+  beqz  a0, 1b                  # loop back if we didn't get a character
+  mv    ra, t2                  # restore return address
+  ret                           # RET: a0 holds the char
+
+
+print_word:                     # print_word: prints word in a0 in hex
+  mv    t4, ra                  # TODO: fix this horrible hack to work around the stack not working. putchar won't clobber t4
+  mv    t0, a0                  # t0 <- ! original argument
+  li    t1, 0xF0000000
+  la    t2, hextbl              # t2 <- 0 hex table
+  li    t3, 28                  # t3 <- ! shift amount
+1:                              # LOOP:
+  and   a0, a0, t1              # apply mask to a0
+  srl   a0, a0, t3              # shift over masked nibble to LSB
+  add   a0, a0, t2              # get address of character in table
+  lb    a0, 0(a0)               # a0 <- hextbl[a0]
+  call  putchar                 # print out char
+  beqz  t3, 2f                  # jump to RET if done
+  srli  t1, t1, 4               # shift mask over
+  addi  t3, t3, -4              # subtract 4 from shift amount
+  mv    a0, t0                  # restore original number
+  j     1b
+2:                              # RET:
+  mv    ra, t4                  # restore the stack
+  ret
+
   
+
 .section .data
 
 hello:
@@ -84,3 +108,9 @@ msg:
 
 nl: 
   .asciz "\r\n"
+
+dbg:  
+  .asciz "DEBUG"
+
+hextbl:
+  .ascii "0123456789ABCDEF"
